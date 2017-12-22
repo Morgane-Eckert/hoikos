4977cb1aad2196ae2460ef27440cdc81aee810e8
charlotteisambert <34058526+charlotteisambert@users.noreply.github.com>
2017-12-22 10:22:04 +0100

    Create m_ajout_utilisateur.php

---
+++ modeles/m_ajout_utilisateur.php
@@ -0,0 +1,53 @@
+<?php /*On ajoute l'article à la base de donnée*/
+session_start();
+
+function connexion_bdd(){
+	try
+	{
+    	$bdd = new PDO('mysql:host=localhost;dbname=hoikos;charset=utf8', 'root', '');
+    	return $bdd;
+	}
+	catch(Exception $e)
+	{
+        die('Erreur : '.$e->getMessage());
+	}
+}
+
+function ajout_utilisateur($type_utilisateur,$nom_utilisateur,$prenom_utilisateur,$telephone_1_utilisateur,$date_de_naissance_utilisateur,$adresse_mail_utilisateur,$mot_de_passe_utilisateur){
+	$bdd=connexion_bdd();
+	$mailexist=0;
+	$entrees = $bdd->query('SELECT adresse_mail_utilisateur FROM utilisateur');
+	while ($mailcheck = $entrees->fetch()){
+		if ($mailcheck[0] == $_POST["adresse_mail"]){
+		$mailexist=1;
+		break;
+		}
+	}
+
+	/*if ((preg_match('#^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).{8,}$#', $_POST["mot_de_passe"])) && ($_POST["mot_de_passe2"] == $_POST["mot_de_passe"] ) && ($mailexist == 0)){*/
+			$requete = $bdd->prepare("INSERT INTO `utilisateur` (`ID_utilisateur`, `ID_logement`, `type_utilisateur`, `nom_utilisateur`, `prenom_utilisateur`, `telephone_1_utilisateur`, `date_de_naissance_utilisateur`, `adresse_mail_utilisateur`, `mot_de_passe_utilisateur`, `date_d_ajout_utilisateur`) VALUES (NULL, NULL, :type_utilisateur, :nom_utilisateur, :prenom_utilisateur, :telephone_1_utilisateur, :date_de_naissance_utilisateur, :adresse_mail_utilisateur, PASSWORD(:mot_de_passe_utilisateur),NOW());");
+			/*On crée une ligne utilisateur*/
+			$affectedLines = $requete->execute(array(
+			'type_utilisateur' => $type_utilisateur,
+	    	'nom_utilisateur' => $nom_utilisateur,
+	    	'prenom_utilisateur' => $prenom_utilisateur,
+	    	'telephone_1_utilisateur' => $telephone_1_utilisateur,
+	    	'date_de_naissance_utilisateur' => $date_de_naissance_utilisateur,
+	    	'adresse_mail_utilisateur' => $adresse_mail_utilisateur,
+	    	'mot_de_passe_utilisateur' => $mot_de_passe_utilisateur
+	    	));
+	    	$_SESSION["prenom_utilisateur"]=$_POST["prenom"];
+			/*On récupère l'ID de l'utilisateur qui vient d'être ajouté*/
+			$reponse1 = $bdd->query('SELECT ID_utilisateur FROM utilisateur ORDER BY ID_utilisateur DESC LIMIT 1');
+			$donnees1 = $reponse1->fetch();
+			/*On le met dans une variable session pour qu'il soit accessible depuis da'utres pages du site*/
+			$_SESSION['id_utilisateur'] = $donnees1['ID_utilisateur'];
+			echo 'bien';
+		/*}
+	
+	else{
+			echo 'pas bien';
+		}*/
+}
+
+?>
