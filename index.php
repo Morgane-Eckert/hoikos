9da3ce01d2e84b6ef52d0e218253f4483cdd1139
charlotteisambert <34058526+charlotteisambert@users.noreply.github.com>
2018-01-05 12:46:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,26 +2,69 @@
 session_start();
 
 if (isset($_GET['target'])) {
-	if ($_GET['target']=='inscription'){	//inscription
+	if ($_GET['target']=='inscription'){
 		include('controleurs/c_inscription.php');
 		if (isset($_GET['action'])){
-    		if ($_GET['action']=='inscription_utilisateur') {
-        		ajout_utilisateur2("1",$_POST["nom"],$_POST["prenom"],$_POST["telephone1"], $_POST["date_naissance"],$_POST["adresse_mail"],$_POST["mot_de_passe"]);
-    		} elseif ($_GET['action']=='inscription_logement') {
-    			echo "inscription logement";
-    		}
-    	} else {
-				inscription_utilisateur();
-		}
+    		if ($_GET['action']=='utilisateur') {
+    			if (isset($_GET['reaction'])){
+    				if ($_GET['reaction']=='rempli') {
+        				ajout_utilisateur2("1",$_POST["nom"],$_POST["prenom"],$_POST["telephone1"], $_POST["date_naissance"],$_POST["adresse_mail"],$_POST["mot_de_passe"]);
+        			}
+        		} else {
+        			inscription_utilisateur();
+        		}
+    		} elseif ($_GET['action']=='logement') {
+    			if (isset($_GET['reaction'])){
+    				if ($_GET['reaction']=='rempli') {
+        				ajout_logement2($_POST["superficie"],$_SESSION['type_logement'],$_POST["telephone_fixe"],$_POST["numero_rue_logement"],$_POST["nom_rue_logement"],$_POST["code_postale_logement"],$_POST["ville_logement"],$_POST["pays_logement"]);
+        			}
+        		} else {
+        			inscription_logement();
+        		}
+    		} elseif ($_GET['action']=='utilisateurs_secondaires') {
+    			if (isset($_GET['reaction'])){
+    				if ($_GET['reaction']=='rempli') {
+    					ajout_utilisateurs_secondaires2("2",$_POST["nom"],$_POST["prenom"],$_POST["telephone1"], $_POST["date_naissance"],$_POST["adresse_mail"],$_POST["mot_de_passe"]);
+        			}
+        		} else {
+        			inscription_utilisateurs_secondaires();
+        		}
+        	} elseif ($_GET['action']=='autres_utilisateurs_secondaires') {
+    			if (isset($_GET['reaction'])){
+    				if ($_GET['reaction']=='rempli') {
+    					ajout_utilisateurs_secondaires2("2",$_POST["nom"],$_POST["prenom"],$_POST["telephone1"], $_POST["date_naissance"],$_POST["adresse_mail"],$_POST["mot_de_passe"]);
+        			}
+        		} else {
+        			inscription_autres_utilisateurs_secondaires();
+        		}
+        	}
+    	}
 	} elseif ($_GET['target']=='decouverte'){	//decouverte
 		include ('controleurs/c_decouverte.php');
 		decouverte();
 	} elseif ($_GET['target']=='compte'){	//connexion
 		include('controleurs/c_connexion.php');
 		if (isset($_GET['action'])){
-			if ($_GET['action']=='connecte') {
-				accueil();
-			}
+			if ($_GET['action']=='connecte') {//L'adresse mail et le mot de passe sont corrects
+                if (isset($_GET['reaction'])){
+                    if ($_GET['reaction']=='nouvel_onglet') {
+                        formulaire_nouvel_onglet();
+                    } else if ($_GET['reaction']=='nouvel_onglet_rempli') {
+                        ajout_nouvel_onglet2($_POST["nom_salle"],$_POST["superficie_salle"]);
+                    } else if ($_GET['reaction']=='home') {
+                        accueil_home();
+                    } else if ($_GET['reaction']=='nouvelle_fonction') {
+                        formulaire_nouveau_capteur();
+                    } else if ($_GET['reaction']=='nouvelle_fonction_rempli') {
+                        ajout_nouveau_capteur2($_POST["nom_capteur"]);
+                    }
+                } else {
+                    accueil_home();
+                }
+			} elseif ($_GET['action']=='mot_de_passe_incorrect' or $_GET['action']=='adresse_mail_inconnue'){//L'adresse mail et/ou le mot de passe sont incorrects, on renvoie vers la page d'accueil
+                include('controleurs/c_premiere_page.php');
+                afficher_premiere_page();
+            }
 		} else{
 			verification2($_POST["adressemail"],$_POST["mot_de_passe"]);
 		}
