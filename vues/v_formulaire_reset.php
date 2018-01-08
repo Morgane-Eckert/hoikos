2354df7e4b83c87d981ccffe4a3e153b65d6a44b
AyFlorant <34058489+AyFlorant@users.noreply.github.com>
2018-01-08 20:28:05 +0100

    Add files via upload

---
+++ vues/v_formulaire_reset.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+
+<HTML>
+<head>
+	<title>Oublie du mot de passe</title>
+	<meta charset="utf-8">
+</head>
+
+<body>
+	<form action="../controleurs/c_mail_reset.php" method="post">
+	
+	<label for="nom">Votre adresse mail:</label> <input type="email" name="mail"><br>
+
+	<?php if ($_SESSION["mailexist"]==0)
+	{
+	?>
+		<span>Adresse mail non existante.</span><br/><br/> 
+	<?php 
+	}
+	?>
+
+	<input type="submit" value="valider">
+	
+	</form>
+</body>
+</HTML>
---
+++ vues/v_mdp_reset.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+
+<HTML>
+<head>
+	<title>Réinitialisation du mot de pase</title>
+	<meta charset="utf-8">
+</head>
+
+<body>
+	<form action="../controleurs/c_mdp_reset.php" method="post">
+
+  	<input type="hidden" name="token" value="<?php echo "".$token."" ?>"></input>
+  	<input type="hidden" name="id" value="<?php echo "".$id."" ?>"></input>
+
+	<label for='mot_de_passe'>Nouveau mot de passe : </label><br/>
+	<input type='password' name='mdp' required><br><br />
+
+	<label for='mot_de_passe2'>Confirmation du mot de passe : </label><br/>
+	<input type='password' name='mdp2' required><br><br />
+			
+	<?php 
+		if ($_SESSION["critmdp"]==1)
+		{
+	?>
+		<span>Le mot de passe doit contenir au moins 8 caractères dont une majuscule, une miniscule, un chiffre et un caractère spécial.</span><br/><br/>
+	<?php 
+		}
+		else
+		{
+	?>
+		<span>Le mot de passe doit contenir au moins 8 caractères dont une majuscule, une miniscule, un chiffre et un caractère spécial.</span><br/><br/>
+			<?php
+		}
+	?>
+	<?php 
+		if ($_SESSION["samemdp"]==1)
+		{
+	?>
+		<span>Les deux mots de passe doivent être identiques.</span><br/><br/>
+			<?php
+		}
+	?>
+
+	<input type="submit" value="valider">
+
+	</form>
+</body>
+</HTML>
