fb616688b16bd774039fd24ca462c6f68dff6359
AyFlorant <34058489+AyFlorant@users.noreply.github.com>
2018-01-08 20:27:23 +0100

    Add files via upload

---
+++ modeles/m_mail_reset.php
@@ -0,0 +1,51 @@
+<?php
+
+function connexion_bdd()
+{
+		try
+	{
+    	$bdd = new PDO('mysql:host=localhost;dbname=hoikos;charset=utf8', 'root', '');
+    	return $bdd;
+	}
+	catch(Exception $e)
+	{
+        die('Erreur : '.$e->getMessage());
+	}	
+}
+
+function mail_exist($email) //Vérifie l'existance du mail (Return 0 si non existant)
+{
+	$bdd=connexion_bdd();
+	$req = $bdd->prepare("SELECT COUNT(*) AS existant FROM utilisateur WHERE adresse_mail_utilisateur = :mail LIMIT 1");
+	$req->execute(array('mail' => $email));
+
+	$donnees = $req->fetch();
+	
+	return $donnees['existant'];
+}
+
+function token_mdp($email) //Génère un token random et le sotck dans la base de données
+{
+	$token = md5(uniqid(rand(), true));
+	
+	$bdd=connexion_bdd();		
+	$req = $bdd->prepare("UPDATE utilisateur SET token_mdp = :token WHERE adresse_mail_utilisateur = :mail");
+	$req->execute(array(
+		'token' => $token,
+		'mail' => $email
+	));
+
+	return $token;
+}
+
+function id_utilisateur($email) //Récupère l'ID de l'utilisateur(à partir du mail)
+{
+	$bdd=connexion_bdd();
+	$req = $bdd->prepare("SELECT ID_utilisateur FROM utilisateur WHERE adresse_mail_utilisateur = :mail");
+	$req->execute(array('mail' => $email));
+	$donnees = $req->fetch();
+	
+	return $donnees['ID_utilisateur'];
+}
+
+?>
---
+++ modeles/m_mdp_reset.php
@@ -0,0 +1,52 @@
+<?php
+
+function connexion_bdd()
+{
+		try
+	{
+    	$bdd = new PDO('mysql:host=localhost;dbname=hoikos;charset=utf8', 'root', '');
+    	return $bdd;
+	}
+	catch(Exception $e)
+	{
+        die('Erreur : '.$e->getMessage());
+	}	
+}
+
+function token_id_check($token,$id) //Vérification de l'existance et concordance du token et id (Return 0 si pas bon)
+{
+	$bdd=connexion_bdd();
+	$req = $bdd->prepare("SELECT COUNT(*) AS existant FROM utilisateur WHERE token_mdp = :token AND ID_utilisateur = :id LIMIT 1");
+	$req->execute(array(
+		'token' => $token,
+		'id' => $id
+	));
+		
+	$donnees = $req->fetch();
+
+	return $donnees['existant'];
+}
+
+function change_mdp($mdp,$token,$id) //Modification du mot de passe dans la BDD
+{
+	$bdd=connexion_bdd();
+	$req = $bdd->prepare("UPDATE utilisateur SET mot_de_passe_utilisateur = PASSWORD(:mdp) WHERE token_mdp = :token AND ID_utilisateur = :id");
+	$req->execute(array(
+		'mdp' => $mdp,
+		'token' => $token,
+	    'id' => $id
+	));	
+}
+
+function reset_token($id) //Enlève le token de la BDD
+{
+	$bdd=connexion_bdd();
+	$req = $bdd->prepare("UPDATE utilisateur SET token_mdp = :token WHERE ID_utilisateur = :id");
+	$req->execute(array(
+		'token' => NULL,
+	    'id' => $id
+	));
+}
+
+
+?>
