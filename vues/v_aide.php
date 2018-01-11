778ca29f0c7b344da3aae41d688620cd7d01c957
AyFlorant <34058489+AyFlorant@users.noreply.github.com>
2018-01-11 13:31:58 +0100

    Add files via upload

--- vues/v_aide.php
+++ vues/v_aide.php
@@ -11,7 +11,9 @@
 		<nav>
 			<a href="index.php" class="Onglet">Retour</a>
 		</nav>		
-		<section>
+		
+		<section class= "overall">
+			<section class= "right">
 			<article class="articlea">
 				<div class="titre"> F.A.Q.</div><br/><!-- Titre dans le bandeau rouge-->
 				<div class="corps"> <!-- Tout ce qu'il y a dans le rectangle blanc-->
@@ -33,16 +35,49 @@
 						Aller Home ou bien dans la pièce désirée, cliquer sur le bouton « Ajouter une fonction ».<br/><br/></p>
 				</div>
 			</article>
+			<a href="" class="boutonb">Voir le tutoriel</a>
+			</section>
+			
+			<section class= "right">
 			<article class="articleb">
 				<div class="titre"> Nos coordonnées :</div><br/><!-- Titre dans le bandeau rouge-->
-				<div class="corps"> <!-- Tout ce qu'il y a dans le rectangle blanc-->
-					<p><strong>Téléphone : </strong>01 02 03 04 05 <br/><br/><strong>Mail :</strong> hoikos@gmail.com</p>
+				<div class="corps2"> <!-- Tout ce qu'il y a dans le rectangle blanc-->
+					<p><strong>Téléphone : </strong>06 59 91 80 18 &ensp;&ensp;&ensp;&ensp;                                       <strong>Adresse :</strong> 10, rue de Vanves Issy-les-Moulineaux (92130)</p>
 				</div>
 			</article>
-<a class="boutona" href = "mailto:hoikos@gmail.com,">Nous contacter par mail</a>
-			<a href="index.php?target=mentions_legales" class="boutonb">Voir le tutoriel</a>
+
+			<article class="articlec">
+			<div class="titre"> Contactez-nous :</div><br/><!-- Titre dans le bandeau rouge-->
+			<form action="index.php?target=aide_message" method="post">
+			<section class="form">
+			<label for="prenom"><strong>Prénom&ensp;</strong></label> <input class="input" type="text" name="prenom" required>&ensp;&ensp;&ensp;&ensp;
+			<label for="nom"><strong>Nom&ensp;</strong></label> <input class="input" type="text" name="nom" required><br/>
+			<p><label for="mail"><strong>Adresse Mail&ensp;</strong></label> <input class="input" type="email" name="mail" required><br/></p>
+			<p><strong class="vmessage">Votre message</strong></p>
+			<textarea class="inputm" name="message" rows='8' cols='60' required></textarea><br/> 
+
+			<?php if ($_GET['target']=='aide_message_envoyé')
+			{
+				echo "Votre message a bien été envoyé !";
+			}
+			?>
+
+			<?php if ($_GET['target']=='aide_message_bug')
+			{
+				echo "Erreur ! Veuillez contacter directement hoikosg4c@gmail.com";
+			}
+			?>
+
+			<input class="boutonc" type="submit" value="Envoyer">
+			</section>
+			</form>
+			</article>
+
 		</section>
+		</section>
+		
 		<?php include("vues/v_footer.php"); ?>
 		
 	</body>
 </html>
+
