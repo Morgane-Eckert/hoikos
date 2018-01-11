de7c3c12cbb1cbfd5c89c936af9391a24a0aec3f
AyFlorant <34058489+AyFlorant@users.noreply.github.com>
2018-01-11 13:32:33 +0100

    Add files via upload

--- controleurs/c_aide.php
+++ controleurs/c_aide.php
@@ -4,4 +4,49 @@ function aide(){
 	include ('vues/v_aide.php');
 }
 
+if (isset($_POST['prenom']) AND isset($_POST['nom']) AND isset($_POST['mail']) AND isset($_POST['message']))
+{
+	//Recuperation des données
+	$prenom = htmlentities($_POST['prenom']);
+	$nom = htmlentities($_POST['nom']);
+	$mail = htmlentities($_POST['mail']);
+	$message = htmlentities($_POST['message']);
+
+	//Envoi du message sur la boite mail d'Hoikos
+	$destinataire = 'hoikosg4c@gmail.com';
+	$expediteur = 'hoikosg4c@gmail.com';
+	$copie = '';
+	$copie_cachee = '';
+	$objet = 'Message de '.$prenom.' '.$nom; // Objet du message
+	$headers  = 'MIME-Version: 1.0' . "\n"; // Version MIME
+	$headers .= 'Content-type: text/html; charset=utf-8' . "\n"; //Pour avoir un mail en format HTML
+	$headers .= 'Reply-To: '.$mail."\n"; // Mail de reponse
+	$headers .= 'From: '.$prenom.' '.$nom.'<'.$mail.'>'."\n"; // Expediteur
+	$headers .= 'Delivered-to: '.$destinataire."\n"; // Destinataire
+	$headers .= 'Cc: '.$copie."\n"; // Copie Cc
+	$headers .= 'Bcc: '.$copie_cachee."\n\n"; // Copie cachée Bcc        
+
+	$texte = 
+	'<html>
+      <body>
+       <p>Adresse mail de '.$prenom.' '.$nom.': '.$mail.'</p> 
+       <p>Contenu du message :</p>
+       <p>'.$message.'</p>
+      </body>
+     </html>';
+
+	if (mail($destinataire, $objet, $texte, $headers)) // Envoi du message
+	{
+	    header("location: index.php?target=aide_message_envoyé");
+	}
+	else // Non envoyé
+	{
+	    header("location: index.php?target=aide_message_bug");
+
+	}
+}
+
+
+
+
 ?>
