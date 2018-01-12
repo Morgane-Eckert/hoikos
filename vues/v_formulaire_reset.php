79a7cc3dbcc1e6c6b849cef101a2bd5755e10fdb
AyFlorant <34058489+AyFlorant@users.noreply.github.com>
2018-01-12 11:52:54 +0100

    Add files via upload

--- vues/v_formulaire_reset.php
+++ vues/v_formulaire_reset.php
@@ -1,26 +1,44 @@
-<!DOCTYPE HTML>
-
-<HTML>
-<head>
-	<title>Oublie du mot de passe</title>
-	<meta charset="utf-8">
-</head>
-
-<body>
-	<form action="./controleurs/c_mail_reset.php" method="post">
+<!DOCTYPE html>
+<html>
 	
-	<label for="nom">Votre adresse mail:</label> <input type="email" name="mail"><br>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="public/css/mail_reset.css">
+		<link rel="stylesheet" href="public/css/footer.css">
+	</head>
+	<body>
+		<header>
+			<a href="index.php" >
+				<img alt="Logo"  src="public/images/logooo.png" id="Logo"><!--Photo du logo et du slogan-->
+			</a>
+		</header>
 
-	<?php if ($_SESSION["mailexist"]==0)
-	{
-	?>
-		<span>Adresse mail non existante.</span><br/><br/> 
-	<?php 
-	}
-	?>
+		<section>
+		<article id="formulaire">
 
-	<input type="submit" value="valider">
-	
-	</form>
-</body>
-</HTML>
+		<?php if ($_GET["target"]=="mail_non_exist")
+		{
+		?>
+			<span class="wrong">Adresse mail non existante</span><br/>
+		<?php 
+		}
+		?>
+
+		<?php if ($_GET["target"]=="mail_sent")
+		{
+		?>
+			<span class="good"><br/>Un mail vous a été envoyé afin de réinitialiser votre mot de passe</span><br/>
+		<?php 
+		}
+		?>
+
+		<form action="./controleurs/c_mail_reset.php" method="post">
+		<input type="email" name="mail" placeholder="Adresse mail" class="input" size="27"><br>
+		<input type="submit" value="Envoyer" class="Onglet">
+		</form>
+
+		</article>
+		</section>
+		<?php include("vues/v_footer.php"); ?>
+	</body>
+</html>
--- vues/v_mdp_reset.php
+++ vues/v_mdp_reset.php
@@ -1,48 +1,98 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
+<html>
+	
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="../public/css/mdp_reset.css">
+		<link rel="stylesheet" href="../public/css/footer.css">
+	</head>
+	<body>
+		<header>
+			<a href="../index.php" >
+				<img alt="Logo"  src="../public/images/logooo.png" id="Logo"><!--Photo du logo et du slogan-->
+			</a>
+		</header>
 
-<HTML>
-<head>
-	<title>Réinitialisation du mot de pase</title>
-	<meta charset="utf-8">
-</head>
-
-<body>
-	<form action="../controleurs/c_mdp_reset.php" method="post">
-
-  	<input type="hidden" name="token" value="<?php echo "".$token."" ?>"></input>
-  	<input type="hidden" name="id" value="<?php echo "".$id."" ?>"></input>
-
-	<label for='mot_de_passe'>Nouveau mot de passe : </label><br/>
-	<input type='password' name='mdp' required><br><br />
-
-	<label for='mot_de_passe2'>Confirmation du mot de passe : </label><br/>
-	<input type='password' name='mdp2' required><br><br />
-			
-	<?php 
-		if ($_SESSION["critmdp"]==1)
+		<section>
+		
+		<?php if ($_SESSION["mdp_changed"]==0)
 		{
-	?>
-		<span>Le mot de passe doit contenir au moins 8 caractères dont une majuscule, une miniscule, un chiffre et un caractère spécial.</span><br/><br/>
-	<?php 
+		?>
+		
+		<p><article id="formulaire">	
+
+		<form action="../controleurs/c_mdp_reset.php" method="post">
+
+	  	<input type="hidden" name="token" value="<?php echo "".$token."" ?>"></input>
+	  	<input type="hidden" name="id" value="<?php echo "".$id."" ?>"></input>
+
+		<label for='mot_de_passe'>Nouveau mot de passe</label><br/>
+		<input type='password' name='mdp' class= "input" required><br><br />
+
+		<label for='mot_de_passe2'>Confirmation du mot de passe</label><br/>
+		<input type='password' name='mdp2' class = "input" required><br><br />
+		</p>
+		<p>
+		<?php 
+			if ($_SESSION["critmdp"]==1)
+			{
+		?>
+			<span class= "wrong">Le mot de passe doit contenir au moins 8 caractères dont une majuscule, une miniscule, un chiffre et un caractère spécial</span><br/><br/>
+		<?php 
+			}
+			else if ($_SESSION["critmdp"]==2)
+			{
+		?>
+			<span class= "good">Le mot de passe doit contenir au moins 8 caractères dont une majuscule, une miniscule, un chiffre et un caractère spécial</span><br/><br/>
+		<?php
+			}
+			else
+			{
+		?>
+			<span class= "normal">Le mot de passe doit contenir au moins 8 caractères dont une majuscule, une miniscule, un chiffre et un caractère spécial</span><br/><br/>
+		<?php
+			}
+		?>
+
+		<?php 
+			if ($_SESSION["samemdp"]==1)
+			{
+		?>
+			<span class= "wrong">Les deux mots de passe doivent être identiques</span><br/><br/>
+				<?php
+			}
+		?>
+		</p>
+		<p>
+		<input type="submit" value="Valider" class="bouton">
+		</form>
+		</p>
+
+		<?php
 		}
 		else
 		{
-	?>
-		<span>Le mot de passe doit contenir au moins 8 caractères dont une majuscule, une miniscule, un chiffre et un caractère spécial.</span><br/><br/>
-			<?php
+		?>
+		<span class= "changed">Votre mot de passe a bien été modifié</span><br/><br/>
+		<?php
 		}
-	?>
-	<?php 
-		if ($_SESSION["samemdp"]==1)
+		?>
+
+
+		</article>
+		</section>
+		
+		<?php if ($_SESSION["mdp_changed"]==0)
 		{
-	?>
-		<span>Les deux mots de passe doivent être identiques.</span><br/><br/>
-			<?php
+		?>
+		<footer>
+		<a href="../index.php?target=conditions_generales" class="lien">Conditions générales d'utilisation</a>
+		<div >Hoikos™ - Tous droits de reproduction réservés</div>
+		<a href="../index.php?target=mentions_legales" class="lien">Mentions légales</a>
+		<a href="../index.php?target=aide"><img src="../public/images/aide.png" id="help" ></a>
+		</footer>
+		<?php
 		}
-	?>
-
-	<input type="submit" value="valider">
-
-	</form>
-</body>
-</HTML>
+		?>
+	</body>
+</html>
