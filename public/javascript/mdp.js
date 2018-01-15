fb4efe822f106da76838c4f15c683c64a5d7b00f
PBidart <32761521+PBidart@users.noreply.github.com>
2018-01-15 02:05:57 +0100

    Add files via upload

---
+++ public/javascript/mdp.js
@@ -0,0 +1,11 @@
+
+function mdpreset(){
+	var mdp1 = prompt("Entrer votre mot de passe actuel");
+	var mdp2 = ;
+	
+	if(mdp1!=mdp2){
+		alert("Ce n'est pas votre mot de passe actuel");
+	} else {
+		alert("Un mail viens de vous être envoyé pour réinitialiser votre mot de passe");
+	}
+}
