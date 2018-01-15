4f1716ac33a4103deca0b16c4e9445d455fdfac1
PBidart <32761521+PBidart@users.noreply.github.com>
2018-01-15 02:03:14 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -59,7 +59,21 @@ if (isset($_GET['target'])) {
                             formulaire_nouveau_capteur();
                         } else if ($_GET['reaction']=='nouvelle_fonction_rempli') {
                             ajout_nouveau_capteur2();
-                        } else {
+						} else if ($_GET['reaction']=='profil'){
+							include ('controleurs/c_profil.php');
+							if(isset($_GET['rempli'])){
+								if($_GET['rempli']=='compte'){
+									profil_editer_utilisateur_principal($_SESSION['ID_utilisateur'],$_POST['nom'],$_POST['prenom'],$_POST['telephone1'],$_POST['adresse_mail'],$_POST['date_naissance']);
+								} else if($_GET['rempli']=='adresse'){
+									profil_editer_adresse($_SESSION['ID_utilisateur'],$_POST['nom_rue_logement'],$_POST['ville_logement'],$_POST['numero_rue_logement'],$_POST['code_postale_logement'],$_POST['telephone_fixe']);								
+								} else if($_GET['rempli']=='secondaire'){
+									profil_editer_utilisateur_secondaire();
+								}
+							} else {
+							profil();	
+							}
+	
+						} else {
                             accueil();
                         }
                     } else {
