9574c5157e93e8fd39e74c13944761b65c6ac148
charlotteisambert <34058526+charlotteisambert@users.noreply.github.com>
2018-01-19 16:05:02 +0100

    Add files via upload

---
+++ vues/v_administrateur_slogan.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="public/css/page_utilisateur.css">
+		<link rel="stylesheet" href="public/css/base-header-avec-bouton.css">
+		<link rel="stylesheet" href="public/css/footer.css">
+	</head>
+	
+	<body>
+		<?php include("vues/v_base-header-avec-bouton-deconnexion.php"); ?>
+		<nav>
+            <a href="index.php?target=compte&action=connecte&reaction=conditions_generales" class="Onglet">Conditions générales d'utilisation</a>
+            <a href="index.php?target=compte&action=connecte&reaction=mentions_legales" class="Onglet">Mentions légales</a>
+            <a href="index.php?target=compte&action=connecte&reaction=slogan" class="actuel">Slogan</a>
+            <a href="index.php?target=compte&action=connecte&reaction=types" class="Onglet">Types de salle et de fonction</a>
+        </nav>
+
+		<section>
+			<article>
+                    <div id="corps"> <!-- Tout ce qu'il y a dans le rectangle blanc-->
+                        <p>
+                            c
+                        </p>
+                    </div>
+			</article>
+		</section>
+		<?php include("vues/v_footer.php"); ?>
+	</body>
+</html>
---
+++ vues/v_administrateur_types.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="public/css/page_utilisateur.css">
+		<link rel="stylesheet" href="public/css/base-header-avec-bouton.css">
+		<link rel="stylesheet" href="public/css/footer.css">
+	</head>
+	
+	<body>
+		<?php include("vues/v_base-header-avec-bouton-deconnexion.php"); ?>
+		<nav>
+            <a href="index.php?target=compte&action=connecte&reaction=conditions_generales" class="Onglet">Conditions générales d'utilisation</a>
+            <a href="index.php?target=compte&action=connecte&reaction=mentions_legales" class="Onglet">Mentions légales</a>
+            <a href="index.php?target=compte&action=connecte&reaction=slogan" class="Onglet">Slogan</a>
+            <a href="index.php?target=compte&action=connecte&reaction=types" class="actuel">Types de salle et de fonction</a>
+        </nav>
+
+		<section>
+			<article>
+                    <div id="corps">
+                        <p>
+                            d
+                        </p>
+                        
+                    </div>
+			</article>
+		</section>
+		<?php include("vues/v_footer.php"); ?>
+	</body>
+</html>
