402a69befdcec02e796aa5af19dbf3544ce9402c
charlotteisambert <34058526+charlotteisambert@users.noreply.github.com>
2018-01-19 16:03:38 +0100

    Add files via upload

---
+++ controleurs/c_administrateur.php
@@ -0,0 +1,20 @@
+<?php 
+include ('modeles/m_administrateur.php');
+
+function administrateur_conditions_generales(){
+    include ('vues/v_administrateur_cgu.php');
+}
+
+function administrateur_mentions_legales(){
+    include ('vues/v_administrateur_mentions_legales.php');
+}
+
+function administrateur_slogan(){
+    include ('vues/v_administrateur_slogan.php');
+}
+
+function administrateur_types(){
+    include ('vues/v_administrateur_types.php');
+}
+
+?>
