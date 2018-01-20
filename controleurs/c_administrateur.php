019fb693a29c65a3c3621f4cf9d9734460763ad4
charlotteisambert <34058526+charlotteisambert@users.noreply.github.com>
2018-01-20 11:01:18 +0100

    Add files via upload

--- controleurs/c_administrateur.php
+++ controleurs/c_administrateur.php
@@ -17,4 +17,42 @@ function administrateur_types(){
     include ('vues/v_administrateur_types.php');
 }
 
+function firstlettertoupper($input){
+    for ($i=0;$i<strlen($input);$i++)   
+    {  
+        if ((preg_match("/^[A-Z]+$/",$input[$i])==true) && ($input[$i-1]!="-"))
+        {
+            $input[$i]=strtolower($input[$i]);
+        }
+        if ($input[$i]=="-")
+        {
+            $input[$i+1]=strtoupper($input[$i+1]);
+        }
+    }
+    $input[0]=strtoupper($input[0]);
+    return $input;
+}
+
+function nouveau_type_de_salle2($nom_nouveau_type_de_salle){
+	$affectedLines = nouveau_type_de_salle($nom_nouveau_type_de_salle);
+	if ($affectedLines === false) {
+        die('Impossible d\'ajouter le nouveau nom de type de fonction !');
+    } else if ($affectedLines == 'oui') {
+    	header("Location: index.php?target=compte&action=connecte&reaction=types&anticipation=nouveau_type_de_salle_deja_existant");
+    } else{
+        header("Location: index.php?target=compte&action=connecte&reaction=types&anticipation=nouveau_type_de_salle_rempli");
+    }
+}
+
+function nouveau_type_de_capteur2($nom_nouveau_type_de_capteur){
+	$affectedLines = nouveau_type_de_capteur($nom_nouveau_type_de_capteur);
+	if ($affectedLines === false) {
+        die('Impossible d\'ajouter le nouveau nom de type de fonction !');
+    } else if ($affectedLines == 'oui') {
+    	header("Location: index.php?target=compte&action=connecte&reaction=types&anticipation=nouveau_type_de_capteur_deja_existant");
+    } else{
+        header("Location: index.php?target=compte&action=connecte&reaction=types&anticipation=nouveau_type_de_capteur_rempli");
+    }
+}
+
 ?>
