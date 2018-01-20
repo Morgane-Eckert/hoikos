09013cc6ed5c40315a9c0ca55d6b05e6915e3df7
Michou <34058527+Mitisman@users.noreply.github.com>
2018-01-20 12:32:13 +0100

    Add files via upload

--- vues/v_administrateur_cgu.php
+++ vues/v_administrateur_cgu.php
@@ -1,3 +1,17 @@
+<!--
+	<form method='post' action='index.php?target=compte&action=connecte&reaction=profil&rempli=compte'>
+		<label for='nom'> Mentions légales : </label><br>
+	    <input type='text' name='nom' id='nom' value ="<?php echo $mentions_legales;?>" required><br><br>
+	    <input type='submit' value='Valider les changements' id='bouton'>
+	</form>
+
+	<form method='post' action='index.php?target=compte&action=connecte&reaction=profil&rempli=compte'>
+		<label for='nom'> Conditions générales : </label><br>
+	    <input type='text' name='nom' id='nom' value ="<?php echo $nom;?>" required><br><br>
+	    <input type='submit' value='Valider les changements' id='bouton'>
+	</form>
+-->
+
 <!DOCTYPE html>
 <html>
 	
@@ -6,6 +20,8 @@
 		<link rel="stylesheet" href="public/css/page_utilisateur.css">
 		<link rel="stylesheet" href="public/css/base-header-avec-bouton.css">
 		<link rel="stylesheet" href="public/css/footer.css">
+		<link rel="stylesheet" href="public/css/admin.css">
+		<link rel="stylesheet" href="public/css/page_administrateur.css">
 	</head>
 	
 	<body>
@@ -20,9 +36,31 @@
 		<section>
 			<article>
                     <div id="corps"> <!-- Tout ce qu'il y a dans le rectangle blanc-->
-                        <p>
-                            a
-                        </p>
+                
+						<?php 
+						$conditions_generales = get_conditions_generales();
+
+						if(isset($_GET['rempli'])){
+							if($_GET['rempli']=='administrateur'){
+							?>
+
+	                        <div style="text-align:center">
+	                            <form method='post' action='index.php?target=compte&action=connecte&reaction=conditions_generales&rempli=OK'>
+									<label for='nom' class="titre"><br> Conditions générales </label><br><br>
+									<textarea rows="50" cols="130" name='conditions_generales' id='conditions_generales'><div><?php echo $conditions_generales;?></div></textarea><br><br>
+								    <input type='submit' value='Valider les changements' id='bouton'><br>
+								</form>
+	                        </div>
+
+	                        <?php 
+							}
+						} 
+						else{
+
+							echo "<a href = 'index.php?target=compte&action=connecte&reaction=conditions_generales&rempli=administrateur' class='editer'>Editer</a></br>".$conditions_generales."</br>";
+						}
+						?>
+
                     </div>
 			</article>
 		</section>
