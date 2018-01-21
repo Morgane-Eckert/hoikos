25ae4d459273462f0b96ecbd885e6136a636eff6
AyFlorant <34058489+AyFlorant@users.noreply.github.com>
2018-01-21 17:11:02 +0100

    Update v_base-header-sans-bouton-deconnexion.php

--- vues/v_base-header-sans-bouton-deconnexion.php
+++ vues/v_base-header-sans-bouton-deconnexion.php
@@ -2,10 +2,6 @@
 	<a href="index.php" >
 		<img alt="Logo"  src="public/images/logo.png" id="Logo"><!--Photo du logo-->
 	<br/>
-	<p class="slogan">
-	<?php 
-	include ('modeles/m_slogan.php');
-	echo slogan(); ?><p>
 	</a>
 	<img alt="Maisons"  src="public/images/petitesmaisons.png" id="Maisons">
 </header>
