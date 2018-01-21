837705027b0f56785a692726485da29ad25ceae8
AyFlorant <34058489+AyFlorant@users.noreply.github.com>
2018-01-21 17:07:06 +0100

    Update v_base-header-avec-bouton-deconnexion.php

--- vues/v_base-header-avec-bouton-deconnexion.php
+++ vues/v_base-header-avec-bouton-deconnexion.php
@@ -1,13 +1,8 @@
 <header>
-	<img alt="Logo"  src="public/images/logo.png" id="Logo"><!--Photo du logo-->
-	<br/>
-	<p class="slogan">
-	<?php 
-	include ('modeles/m_slogan.php');
-	echo slogan(); ?><p>
-	<img alt="Maisons"  src="public/images/petitesmaisons.png" id="Maisons">
-	<span class="nom_compte">
-		<?php echo $_SESSION["prenom_utilisateur"]; ?>
-		<a href="index.php?target=deconnexion" class="deconnex">Déconnexion</a>
-	</span>
-</header>
++	<img alt="Logo"  src="public/images/logoo.png" id="Logo"><!--Photo du logo et du slogan-->
+  	<img alt="Maisons"  src="public/images/petitesmaisons.png" id="Maisons">
+  	<span class="nom_compte">
+  		<?php echo $_SESSION["prenom_utilisateur"]; ?>
+ 		<a href="index.php?target=deconnexion" class="deconnex">Déconnexion</a>
+ 	</span>
+ </header>
