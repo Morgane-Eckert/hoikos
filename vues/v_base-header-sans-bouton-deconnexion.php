a9a296b978202759e1a137230d8b8b2e9f1312dd
AyFlorant <34058489+AyFlorant@users.noreply.github.com>
2018-01-23 21:47:22 +0100

    Delete v_base-header-sans-bouton-deconnexion.php

--- vues/v_base-header-sans-bouton-deconnexion.php
+++
@@ -1,7 +0,0 @@
-<header>
-	<a href="index.php" >
-		<img alt="Logo"  src="public/images/logo.png" id="Logo"><!--Photo du logo-->
-	<br/>
-	</a>
-	<img alt="Maisons"  src="public/images/petitesmaisons.png" id="Maisons">
-</header>
