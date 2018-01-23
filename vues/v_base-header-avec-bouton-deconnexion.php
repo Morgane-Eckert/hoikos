bb1291ab2c0616d0e5a26b58ef2f15140e8893a8
AyFlorant <34058489+AyFlorant@users.noreply.github.com>
2018-01-23 21:47:28 +0100

    Delete v_base-header-avec-bouton-deconnexion.php

--- vues/v_base-header-avec-bouton-deconnexion.php
+++
@@ -1,8 +0,0 @@
-<header>
-+	<img alt="Logo"  src="public/images/logoo.png" id="Logo"><!--Photo du logo et du slogan-->
-  	<img alt="Maisons"  src="public/images/petitesmaisons.png" id="Maisons">
-  	<span class="nom_compte">
-  		<?php echo $_SESSION["prenom_utilisateur"]; ?>
- 		<a href="index.php?target=deconnexion" class="deconnex">Déconnexion</a>
- 	</span>
- </header>
