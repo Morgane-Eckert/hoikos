1d265967358de08bdfddc151203814e3d8d1d5d3
PBidart <32761521+PBidart@users.noreply.github.com>
2018-01-24 19:07:22 +0100

    Update v_ajout_utilisateurs_secondaires.php

--- vues/v_ajout_utilisateurs_secondaires.php
+++ vues/v_ajout_utilisateurs_secondaires.php
@@ -1,25 +1,35 @@
 <!DOCTYPE html>
 <html>
-    
+
     <head>
         <meta charset="utf-8">
         <link rel="stylesheet" href="public/css/inscription_secondaire.css">
         <link rel="stylesheet" href="public/css/footer.css">
     </head>
-    
+
     <body>
-        <?php   include("vues/v_header_no_bouton.php"); 
+        <?php   include("vues/v_header_no_bouton.php");
                 verif_cond_utilisateur();
         ?>
         <script src="public/js/valider_formulaire_inscription.js" ></script> <!-- Verifications formulaire en JS -->
         <nav>
-            <a href="index.php" class="Onglet">Retour</a>
+            <?php
+              if(isset($_GET["profil"])){
+                ?>
+                <a href="index.php?target=compte&action=connecte&reaction=profil" class="Onglet">Profil</a>
+                <?php
+              } else {
+                ?>
+                <a href="index.php" class="Onglet">Retour</a>
+              <?php
+              }
+            ?>
         </nav>
-        
+
         <section>
             <article>
                 <div id="titre">Ajouter un utilisateur secondaire</div><br/>
-                <form id='corps' method='post' action='index.php?target=inscription&action=utilisateurs_secondaires&reaction=rempli' onsubmit="return verifForm(this)">
+                <form id='corps' method='post' action=<?php if(isset($_GET['profil'])){echo "index.php?target=inscription&action=utilisateurs_secondaires&reaction=rempli&profil";}else{echo "index.php?target=inscription&action=utilisateurs_secondaires&reaction=rempli";}?> onsubmit="return verifForm(this)">
                         <label for='nom' id='test'> Nom : </label><br/>
                         <input type='text' pattern='^[a-zA-Z]+$' name='nom' id='nom' required><br><br>
                         <label for='prenom'> Prénom :  </label><br/>
@@ -37,18 +47,26 @@
 
                         <label for='mot_de_passe2'>Confirmation du mot de passe : </label><br/>
                         <input type='password' name='mot_de_passe2' id='mot_de_passe2'  required><br><br />
-    
+
                         <label for='Conditions'></label><input type='checkbox'  id='Conditions' required><a href='index.php?target=conditions_generales' id='Condition' target=_blank>J'ai lu et accepté les conditions générales d'utilisation</a><br><br><br/>
                         <input type='submit' value='Valider' id='bouton'>
             </form>
 
             <p id='NB'> Tous les champs sont obligatoires.<br> </p>
             </article>
+            <?php
+            if(isset($_GET["profil"])){
+            echo "";
+            } else {
+            ?>
             <aside>
                 <a href="index.php?target=compte&action=connecte&reaction=home" class="Grandonglet">   Ignorer et finir l'inscription   </a>
             </aside>
+            <?php
+            }
+            ?>
         </section>
         <?php include("vues/v_footer.php"); ?>
-        
+
     </body>
 </html>
