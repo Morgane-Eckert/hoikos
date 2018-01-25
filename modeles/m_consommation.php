36902ca2b4f5200f8e6e349faf36030ce099d964
Mike Belhomme <34034247+Azoth38@users.noreply.github.com>
2018-01-25 19:01:15 +0100

    Add files via upload

---
+++ modeles/m_consommation.php
@@ -0,0 +1,48 @@
+<?php
+
+function connexion_bdd2(){
+	try
+	{
+    	$bdd = new PDO('mysql:host=localhost;dbname=hoikos;charset=utf8', 'root', '');
+    	return $bdd;
+	}
+	catch(Exception $e)
+	{
+        die('Erreur : '.$e->getMessage());
+	}
+}
+
+function afficher_consommation_eau(){
+	$bdd=connexion_bdd2();
+	$reponse=$bdd->prepare('SELECT donnee_recue_capteur from capteur where ID_logement= :ID_logement and ID_type_de_capteur =12');
+	$reponse->execute(array(
+		'ID_logement' => $_SESSION['ID_logement']
+		));
+	
+	$i=0;
+	while ($donnees = $reponse->fetch()){
+	    $consommation_eau[$i]= $donnees['donnee_recue_capteur']; 
+	    $i++; 
+	}
+	return $consommation_eau;
+}
+function afficher_consommation_elec(){
+	$bdd=connexion_bdd2();
+	$reponse=$bdd->prepare('SELECT donnee_recue_capteur from capteur where ID_logement= :ID_logement and ID_type_de_capteur =11');
+	$reponse->execute(array(
+		'ID_logement' => $_SESSION['ID_logement']
+		));
+	$i=0;
+	while ($donnees = $reponse->fetch()){
+	    $consommation_elec[$i]= $donnees['donnee_recue_capteur']; 
+	    $i++; 
+	}
+	return $consommation_elec;
+}
+
+
+//$reponse=$bdd->prepare('SELECT donnee_recue_capteur from capteur where ID_logement= :ID_logement and ID_type_de_capteur =7');
+//	$reponse->execute(array(
+	//	'ID_logment' => $_SESSION['ID_logement']
+	//	));
+?>
