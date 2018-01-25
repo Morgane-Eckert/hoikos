11560b8361640bb59c461e52f5a0e18ca53d62ae
PBidart <32761521+PBidart@users.noreply.github.com>
2018-01-25 18:59:03 +0100

    Update c_profil.php

--- controleurs/c_profil.php
+++ controleurs/c_profil.php
@@ -15,14 +15,15 @@ function profil_secondaire(){
 function profil_editer_utilisateur_principal($id_utilisateur,$nom,$prenom,$telephone,$adresse_mail,$date_de_naissance){
 	$bdd = connexion_bdd();
 
-	$reponse = $bdd->prepare('SELECT COUNT(*) FROM utilisateur WHERE adresse_mail_utilisateur = :adresse_mail_utilisateur');
+	$reponse = $bdd->prepare('SELECT COUNT(*) FROM utilisateur WHERE adresse_mail_utilisateur = :adresse_mail_utilisateur AND ID_utilisateur != :id');
 	$reponse->execute(array(
 	'adresse_mail_utilisateur' => $adresse_mail,
+	'id' => $id_utilisateur
 	));
 
 	$count=$reponse->fetchColumn();
 
-	if($count<=1){
+	if($count==0){
 		$req = $bdd->prepare('UPDATE utilisateur SET nom_utilisateur = :nvnom, prenom_utilisateur = :nvprenom, adresse_mail_utilisateur = :nvadresse, date_de_naissance_utilisateur = :nvdate, telephone_1_utilisateur = :nvtelephone WHERE ID_utilisateur = :id');
 		$req->execute(array(
 			'nvnom' => $nom,
@@ -32,37 +33,49 @@ function profil_editer_utilisateur_principal($id_utilisateur,$nom,$prenom,$telep
 			'nvtelephone' => $telephone,
 			'id' => $id_utilisateur,
 		));
+			header("Location:index.php?target=compte&action=connecte&reaction=profil");
 
+	} else {
+
+		$req = $bdd->prepare('UPDATE utilisateur SET nom_utilisateur = :nvnom, prenom_utilisateur = :nvprenom, date_de_naissance_utilisateur = :nvdate, telephone_1_utilisateur = :nvtelephone WHERE ID_utilisateur = :id');
+		$req->execute(array(
+			'nvnom' => $nom,
+			'nvprenom' => $prenom,
+			'nvdate' => $date_de_naissance,
+			'nvtelephone' => $telephone,
+			'id' => $id_utilisateur,
+		));
+
+		header("Location:index.php?target=compte&action=connecte&reaction=profil&conflit=".$adresse_mail);
 	}
-
-	header("Location:index.php?target=compte&action=connecte&reaction=profil");
 
 }
 
 function profil_editer_adresse($id_utilisateur,$ruelogement,$villelogement,$numeroruelogement,$codepostallogement,$telephonefixe){
 	$bdd = connexion_bdd();
 
-	$reponse = $bdd->prepare('SELECT * FROM utilisateur WHERE ID_utilisateur=:id');
-    $reponse->execute(array(
-    'id'=> $id_utilisateur,
-    ));
+			$reponse = $bdd->prepare('SELECT * FROM utilisateur WHERE ID_utilisateur=:id');
+		  $reponse->execute(array(
+		  'id'=> $id_utilisateur,
+		  ));
 
-    while ($donnees = $reponse->fetch()){
-    $ID_logement = $donnees['ID_logement'];
-	}
-    $reponse->closeCursor();
+		    while ($donnees = $reponse->fetch()){
+		    $ID_logement = $donnees['ID_logement'];
+			}
+		    $reponse->closeCursor();
 
-	$req = $bdd->prepare('UPDATE logement SET nom_rue_logement = :nvnomrue,ville_logement = :nvville, numero_rue_logement = :nvnumerorue, code_postale_logement = :nvcodepostale, telephone_logement = :nvtelephone WHERE ID_logement = :id');
-	$req->execute(array(
-		'nvnomrue' => $ruelogement,
-		'nvville' => $villelogement,
-		'nvnumerorue' => $numeroruelogement,
-		'nvcodepostale' => $codepostallogement,
-		'nvtelephone' => $telephonefixe,
-		'id' => $ID_logement,
-	));
+			$req = $bdd->prepare('UPDATE logement SET nom_rue_logement = :nvnomrue,ville_logement = :nvville, numero_rue_logement = :nvnumerorue, code_postale_logement = :nvcodepostale, telephone_logement = :nvtelephone WHERE ID_logement = :id');
+			$req->execute(array(
+				'nvnomrue' => $ruelogement,
+				'nvville' => $villelogement,
+				'nvnumerorue' => $numeroruelogement,
+				'nvcodepostale' => $codepostallogement,
+				'nvtelephone' => $telephonefixe,
+				'id' => $ID_logement,
+			));
 
-	header("Location:index.php?target=compte&action=connecte&reaction=profil");
+			header("Location:index.php?target=compte&action=connecte&reaction=profil");
+
 }
 
 
