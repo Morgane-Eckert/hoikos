f56ca3b131520d768eb58b8770516d23ed4aa0e7
PBidart <32761521+PBidart@users.noreply.github.com>
2018-01-27 11:10:09 +0100

    Update c_profil.php

--- controleurs/c_profil.php
+++ controleurs/c_profil.php
@@ -1,6 +1,6 @@
 <?php
 
-include ('/modeles/m_profil.php');
+include ('modeles/m_profil.php');
 
 
 
@@ -15,12 +15,7 @@ function profil_secondaire(){
 function profil_editer_utilisateur_principal($session_adresse,$nom,$prenom,$telephone,$adresse_mail,$date_de_naissance){
 	$bdd = connexion_bdd();
 
-	$rep = $bdd->prepare('SELECT * FROM utilisateur WHERE adresse_mail_utilisateur = :adresse');
-	$rep->execute(array(
-	'adresse' => $session_adresse
-	));
-	$donnees = $rep->fetch();
-	$id_utilisateur = $donnees["ID_utilisateur"];
+		$id_utilisateur = id_utilisateur($session_adresse);
 
 	$reponse = $bdd->prepare('SELECT COUNT(*) FROM utilisateur WHERE adresse_mail_utilisateur = :adresse_mail_utilisateur AND ID_utilisateur != :id');
 	$reponse->execute(array(
@@ -62,22 +57,17 @@ function profil_editer_utilisateur_principal($session_adresse,$nom,$prenom,$tele
 function profil_editer_adresse($session_adresse,$ruelogement,$villelogement,$numeroruelogement,$codepostallogement,$telephonefixe){
 	$bdd = connexion_bdd();
 
-	$rep = $bdd->prepare('SELECT * FROM utilisateur WHERE adresse_mail_utilisateur = :adresse');
-	$rep->execute(array(
-	'adresse' => $session_adresse
-	));
-	$donnees = $rep->fetch();
-	$id_utilisateur = $donnees["ID_utilisateur"];
+	$id_utilisateur = id_utilisateur($session_adresse);
 
-			$reponse = $bdd->prepare('SELECT * FROM utilisateur WHERE ID_utilisateur=:id');
+			$reponse = $bdd->prepare('SELECT ID_logement FROM utilisateur WHERE ID_utilisateur=:id');
 		  $reponse->execute(array(
 		  'id'=> $id_utilisateur,
 		  ));
 
-		    while ($donnees = $reponse->fetch()){
-		    $ID_logement = $donnees['ID_logement'];
-			}
-		    $reponse->closeCursor();
+			$donnees = $reponse->fetch();
+
+		  $ID_logement = $donnees['ID_logement'];
+		  $reponse->closeCursor();
 
 			$req = $bdd->prepare('UPDATE logement SET nom_rue_logement = :nvnomrue,ville_logement = :nvville, numero_rue_logement = :nvnumerorue, code_postale_logement = :nvcodepostale, telephone_logement = :nvtelephone WHERE ID_logement = :id');
 			$req->execute(array(
@@ -93,5 +83,70 @@ function profil_editer_adresse($session_adresse,$ruelogement,$villelogement,$num
 
 }
 
+
+function supprimer($id){
+	$bdd=connexion_bdd();
+
+	$reponse = $bdd->prepare('SELECT * FROM utilisateur WHERE id_utilisateur = :id');
+	$reponse->execute(array(
+	'id'=> $id,
+	));
+
+	$nom=$donnees['nom_utilisateur'];
+
+
+
+	$req = $bdd->prepare("DELETE FROM utilisateur WHERE id_utilisateur = :id");
+	$req->execute(array(
+		"id"=>$id,
+	));
+
+	header("Location:index.php?target=compte&action=connecte&reaction=profil&suppression=".$nom);
+}
+
+function ajouter_cemac($id_cemac,$session_adresse){
+	$bdd = connexion_bdd();
+
+	$reponse = $bdd->prepare('SELECT * FROM utilisateur WHERE adresse_mail_utilisateur=:id');
+	$reponse->execute(array(
+	'id'=> $session_adresse,
+	));
+
+	$donnees=$reponse->fetch();
+
+	$req = $bdd->prepare('UPDATE cemac SET etat_cemac = 1, ID_logement = :id WHERE numero_de_cemac = :num');
+	$req->execute(array(
+			'id' => $donnees["ID_logement"],
+			'num'=> $id_cemac
+			));
+}
+
+function changement($email){
+
+			$token = token_mdp($email); //Génère un token random et le stock dans la base de données
+			$id = id_utilisateur($email); //Récupère l'ID de l'utilisateur(à partir du mail)
+			//Envoyer un mail contenant un lien vers la page permettant la modificationtion du mot de passe
+			$destinataire = $email;
+			$expediteur = 'hoikosg4c@gmail.com';
+			$copie = '';
+			$copie_cachee = 'hoikosg4c@gmail.com';
+			$objet = 'Changement du mot de passe Hoikos'; // Objet du message
+			$headers  = 'MIME-Version: 1.0' . "\n"; // Version MIME
+			$headers .= 'Reply-To: '.$expediteur."\n"; // Mail de reponse
+			$headers .= 'From: "Hoikos"<'.$expediteur.'>'."\n"; // Expediteur
+			$headers .= 'Delivered-to: '.$destinataire."\n"; // Destinataire
+			$headers .= 'Cc: '.$copie."\n"; // Copie Cc
+			$headers .= 'Bcc: '.$copie_cachee."\n\n"; // Copie cachée Bcc
+			$message = "Bonjour, Pour changer votre mot de passe Hoikos, cliquez sur le lien suivant pour le réinitialiser : http://localhost/thelast/h/controleurs/c_mdp_reset.php?token=$token&id=$id";
+			if (mail($destinataire, $objet, $message, $headers)) // Envoi du message
+			{
+				echo "";
+			}
+			else // Non envoyé
+			{
+			    echo "Erreur lors de l'envoi du mail, veuillez contacter directement Hoikos";
+			}
+			header("Location:index.php?target=compte&action=connecte&reaction=profil&Mail");
+		}
 
 ?>
