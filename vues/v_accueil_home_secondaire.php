61ccb9f602685e8df89185c7ad7723c2152f30c8
mariealmo <34058470+mariealmo@users.noreply.github.com>
2018-01-27 17:40:26 +0100

    Update v_accueil_home_secondaire.php

--- vues/v_accueil_home_secondaire.php
+++ vues/v_accueil_home_secondaire.php
@@ -37,7 +37,7 @@ include("accueil_onglets.php");
 
 		<section>
 			<article>
-				<div id="titre">Fonctions de la maison<a href="" class="Routine">Routine</a></div> 
+				<div id="titre">Fonctions de la maison<a href="index.php?target=compte&action=connecte&reaction=routine&anticipation=home" class="Routine">Routine</a></div> 
                     <br/><!-- Titre dans le bandeau rouge-->
                     <div id="corps"> <!-- Tout ce qu'il y a dans le rectangle blanc-->
                         <?php //Affichage des onglets
