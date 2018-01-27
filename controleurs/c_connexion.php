491c69152250af8fcb657a6765c800998e862f43
mariealmo <34058470+mariealmo@users.noreply.github.com>
2018-01-27 17:44:43 +0100

    Update c_connexion.php

--- controleurs/c_connexion.php
+++ controleurs/c_connexion.php
@@ -100,4 +100,132 @@ function accueil_suppression_fonction2(){
     
 }
 
+function routine(){
+    include ('vues/v_routine.php');
+}
+
+function formulaire_nouvelle_routine_erreur(){
+    if ($_GET['reaction']=='nouvelle_routine_salle_rempli') {
+        header('Location: index.php?target=compte&action=connecte&reaction=nouvelle_routine_salle&comprehension=erreur&anticipation='.$_GET['anticipation']);
+    }elseif ($_GET['reaction']=='nouvelle_routine_capteur_rempli') {
+        header('Location: index.php?target=compte&action=connecte&reaction=nouvelle_routine_capteur&comprehension=erreur&anticipation='.$_GET['anticipation']);
+    }elseif ($_GET['reaction']=='nouvelle_routine_consigne_rempli') {
+        header('Location: index.php?target=compte&action=connecte&reaction=nouvelle_routine_consigne&comprehension=erreur&anticipation='.$_GET['anticipation']);
+    }elseif ($_GET['reaction']=='nouvelle_routine_horaire_rempli') {
+        header('Location: index.php?target=compte&action=connecte&reaction=nouvelle_routine_horaire&comprehension=erreur&anticipation='.$_GET['anticipation']);
+    }elseif ($_GET['reaction']=='nouvelle_routine_nom_rempli') {
+        header('Location: index.php?target=compte&action=connecte&reaction=nouvelle_routine_nom&comprehension=erreur&anticipation='.$_GET['anticipation']);
+    }
+}
+
+function formulaire_nouvelle_routine_salle(){
+    include ('vues/v_formulaire_nouvelle_routine_salle.php');
+}
+
+function ajout_salle_routine2($salles){
+    $affectedLines = ajout_salle_routine($salles);    
+    if ($affectedLines === false) {
+        die('Impossible d\'ajouter le commentaire !');
+    }
+    else {
+        if(isset($_GET['comprehension'])){
+            header('Location: index.php?target=compte&action=connecte&reaction=routine&anticipation='.$_GET['anticipation']);
+        }else  { 
+            header('Location: index.php?target=compte&action=connecte&reaction=nouvelle_routine_capteur&anticipation='.$_GET['anticipation']);
+        }  
+    }
+}
+
+function formulaire_nouvelle_routine_capteur(){
+    include ('vues/v_formulaire_nouvelle_routine_capteur.php');
+}
+
+function ajout_capteur_routine2($capteurs){
+    $affectedLines = ajout_capteur_routine($capteurs);    
+    if ($affectedLines === false) {
+        die('Impossible d\'ajouter le commentaire !');
+    }
+    else {
+        if(isset($_GET['comprehension'])){
+            header('Location: index.php?target=compte&action=connecte&reaction=routine&anticipation='.$_GET['anticipation']);
+        }else  { 
+        header('Location: index.php?target=compte&action=connecte&reaction=nouvelle_routine_consigne&anticipation='.$_GET['anticipation']);
+        } 
+    }
+}
+
+function formulaire_nouvelle_routine_consigne(){
+    include ('vues/v_formulaire_nouvelle_routine_consigne.php');
+}
+
+function ajout_consigne_routine2($consignes){
+    $affectedLines = ajout_consigne_routine($consignes);    
+    if ($affectedLines === false) {
+        die('Impossible d\'ajouter le commentaire !');
+    }
+    else {
+        if(isset($_GET['comprehension'])){
+        header('Location: index.php?target=compte&action=connecte&reaction=routine&anticipation='.$_GET['anticipation']);
+        }else  { 
+        header('Location: index.php?target=compte&action=connecte&reaction=nouvelle_routine_horaire&anticipation='.$_GET['anticipation']);
+        } 
+    }
+}
+
+function formulaire_nouvelle_routine_horaire(){
+    include ('vues/v_formulaire_nouvelle_routine_horaire.php');
+}
+
+function ajout_horaire_routine2($jours, $debut, $fin){
+    $affectedLines = ajout_horaire_routine($jours,$debut,$fin);    
+    if ($affectedLines === false) {
+        die('Impossible d\'ajouter le commentaire !');
+    }
+    else {
+        if(isset($_GET['comprehension'])){
+        header('Location: index.php?target=compte&action=connecte&reaction=routine&anticipation='.$_GET['anticipation']);
+        }else  { 
+        header('Location: index.php?target=compte&action=connecte&reaction=nouvelle_routine_nom&anticipation='.$_GET['anticipation']);
+        }
+    }
+}
+
+function formulaire_nouvelle_routine_nom(){
+    include ('vues/v_formulaire_nouvelle_routine_nom.php');
+}
+
+function ajout_nom_routine2($nom){
+    $affectedLines = ajout_nom_routine($nom);    
+    if ($affectedLines === false) {
+        die('Impossible d\'ajouter le commentaire !');
+    }elseif ($affectedLines == 'nom déjà existant'){
+        header('Location: index.php?target=compte&action=connecte&reaction=nouvelle_routine_nom&comprehension=erreur2&anticipation='.$_GET['anticipation']);
+    }else {
+        header('Location: index.php?target=compte&action=connecte&reaction=routine&anticipation='.$_GET['anticipation']);
+    }
+}
+
+function effacer_routine2(){
+    $affectedLines = effacer_routine();    
+    if ($affectedLines === false) {
+        die('Impossible d\'ajouter le commentaire !');
+    }
+    else {
+        header('Location: index.php?target=compte&action=connecte&reaction=routine&anticipation='.$_GET['anticipation']);
+    }
+}
+
+function suppression_routine2(){
+    include ('vues/v_suppression_routine.php');
+}
+
+function suppression_routine_confirme2(){
+    $affectedLines = suppression_routine_confirme(); 
+    if ($affectedLines === false) {
+        die('Impossible de supprimer la pièce !');
+    } else {
+        header('Location: index.php?target=compte&action=connecte&reaction=routine&comprehension=confirme&anticipation='.$_GET['anticipation']);
+    }
+    
+}
 ?>
