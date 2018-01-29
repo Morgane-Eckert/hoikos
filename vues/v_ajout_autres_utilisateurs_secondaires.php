7d69a00c937d1e22744c17affa09dece48fb87f7
charlotteisambert <34058526+charlotteisambert@users.noreply.github.com>
2018-01-29 18:32:12 +0100

    Add files via upload

--- vues/v_ajout_autres_utilisateurs_secondaires.php
+++ vues/v_ajout_autres_utilisateurs_secondaires.php
@@ -4,19 +4,18 @@
     <head>
         <meta charset="utf-8">
         <link rel="stylesheet" href="public/css/inscription_secondaire.css">
-        <link rel="stylesheet" href="public/css/base-header-sans-bouton.css">
         <link rel="stylesheet" href="public/css/footer.css">
 
     </head>
     
     <body>
-        <?php include("vues/v_base-header-sans-bouton-deconnexion.php"); ?>
+       <?php include("vues/v_header_no_bouton.php"); ?>
         
         <nav>
-            <a href="index.php" class="Onglet">Retour</a>
+            <a href="index.php?target=inscription&action=utilisateurs_secondaires" class="Onglet">Retour</a>
         </nav>
         
-        <section>
+       <section>
             <article class="petit_article">
                 <div id="titre">Utilisateurs secondaires déjà créés</div>
                 <div class="liste_utilisateurs">
@@ -34,26 +33,27 @@
                 </div>
             </article>  
             <article class="grand_article">
-                <div id="titre">Ajouter un nouvel secondaire</div><br/>
+                <div id="titre">Ajouter un nouvel utilisateur secondaire</div><br/>
                 <form method='post' action='index.php?target=inscription&action=autres_utilisateurs_secondaires&reaction=rempli' id='corps'>
-                        <label for='nom' id='test'> Nom : </label><br/>
-                        <input type='text' name='nom' id='nom' required><br><br>
+                       <label for='nom' id='test'> Nom : </label><br/>
+                        <input type='text' pattern='^[a-zA-Z]+$' name='nom' id='nom' required><br><br>
                         <label for='prenom'> Prénom :  </label><br/>
-                        <input type='text' name='prenom' id='prenom' required><br><br />
-                        <label for='telephone'> Téléphone personnel :  </label><br/>
-                        <input type='text' name='telephone1' id='telephone1' maxlenght='10' required><br><br />
+                        <input type='text' pattern='^[a-zA-Z]+$' name='prenom' id='prenom' required><br><br />
+                        <label for='telephone'> Téléphone portable :  </label><br/>
+                        <input type='tel'  name='telephone1' id='telephone1' maxlength='10' required><br><br />
+                        <?php verif_mail();?>
                         <label for='adresse_mail'> Adresse mail :  </label><br/>
-                        <input type='email' name='adresse_mail' id='adresse_mail'  required><br><br />
+                        <input type='email' name='adresse_mail' size='30' id='adresse_mail'  required><br><br />
                         <label for='date_naissance'> Date de naissance:  </label><br/>
                         <input type='date' name='date_naissance' id='date_naissance'  required><br><br />
-                        <span class="NB2">Le mot de passe doit contenir au moins 8 caractères dont une majuscule, une miniscule, un chiffre et un caractère spécial.</span><br/><br/>
+                        <?php verif_mdp();?>
                         <label for='mot_de_passe'>Mot de passe : </label><br/>
                         <input type='password' name='mot_de_passe' id='mot_de_passe'  required><br><br />
 
                         <label for='mot_de_passe2'>Confirmation du mot de passe : </label><br/>
                         <input type='password' name='mot_de_passe2' id='mot_de_passe2'  required><br><br />
     
-                        <label for='Conditions'></label><input type='checkbox'  id='Conditions' required><a href='conditionsgenerales.html' id='Condition'>J'ai lu et accepté les conditions générales d'utilisation</a><br><br><br/>
+                        <label for='Conditions'></label><input type='checkbox'  id='Conditions' required><a href='index.php?target=conditions_generales' id='Condition' target=_blank>J'ai lu et accepté les conditions générales d'utilisation</a><br><br><br/>
                         <input type='submit' value='Valider' id='bouton'>
             </form>
             <p id='NB'> Tous les champs sont obligatoires.<br></p>
--- vues/v_ajout_logement.php
+++ vues/v_ajout_logement.php
@@ -3,22 +3,22 @@
 	<head>
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="public/css/inscription.css">
-		<link rel="stylesheet" href="public/css/base-header-sans-bouton.css">
 		<link rel="stylesheet" href="public/css/footer.css">
 		<script type="text/javascript" src="public/js/adresse.js"></script>
 	</head>
 	<body>
-		<?php include("vues/v_base-header-sans-bouton-deconnexion.php"); ?>
+		<?php include("vues/v_header_no_bouton.php");
+				verif_cond_logement();
+
+		?>
 		<nav>
-			<a href="index.php" class="Onglet">Retour</a>
+			<a href="index.php?target=inscription&action=utilisateur" class="Onglet">Retour</a>
 		</nav>
 		<section>
 			<article>
 				<div id="titre">Mon logement</div><br/>
 				<form method='post' action='index.php?target=inscription&action=logement&reaction=rempli' id='corps'>
-						<label for='type_logement'>Vous habitez dans : </label>
-						<input type='radio' id='Maison' name='type_logement' value='Maison' required> Une maison
-						<input type='radio' id='Appartement' name='type_logement' value='Appartement' required> Un appartement<br><br>
+						<?php verif_cemac();?>
 						<label for='numero_de_cemac'> Numéro de série de votre CeMAC (ce numéro vous a été fourni en magasin lors de votre premier achat) : </label><br/>
 						<input type='text' name='numero_de_cemac' maxlength="5" required>
 						<br><br>
@@ -27,21 +27,21 @@
 						<label for='telephone_fixe'> Téléphone fixe :  </label><br/>
 						<input type='text' name='telephone_fixe' id='telephone_fixe' maxlength='10' required><br><br />
 
-						<label>Addresse</label><br>
-						<input id="autocomplete" placeholder="Taper votre adresse..."
-             onFocus="geolocate()" type="text" class='adresse' style="width:300px;"><br><br>
+						<label>Adresse</label><br>
+						<input 	id="autocomplete" placeholder="Taper votre adresse..."
+            					onFocus="geolocate()" type="text" class='adresse' style="width:300px;"><br><br>
 
 						<label for='numero_rue_logement' id='numero_rue_logement'> Numéro de rue : </label><br/>
-						<input type='text' name='numero_rue_logement' id='street_number' class='adresse' required disabled = 'true'><br><br>
+						<input type='text' name='numero_rue_logement' id='street_number' class='adresse' required><br><br>
 						<label for='nom_rue_logement' id='nom_rue_logement'> Nom de rue : </label><br/>
-						<input type='text' name='nom_rue_logement' id='route' class='adresse'required disabled = 'true'><br><br>
+						<input type='text' name='nom_rue_logement' id='route' class='adresse' required><br><br>
 						<label for='code_postale_logement' id='code_postale_logement'> Code postal : </label><br/>
-						<input type='text' name='code_postale_logement' id='postal_code' class='adresse' required disabled = 'true'><br><br>
+						<input type='text' name='code_postale_logement' id='postal_code' class='adresse' required><br><br>
 						<label for="ville_logement">Ville :</label><br>
-						<input type='text' name="ville_logement" id="locality" class='adresse' required disabled = 'true'> <br><br>
+						<input type='text' name="ville_logement" id="locality" class='adresse' required> <br><br>
 						<label for="pays_logement">Pays :</label><br>
-						<input type='text' name="pays_logement" id="country" class='adresse' required disabled = 'true'> <br><br>
-						<input type='submit' value='Valider' id='bouton' onclick='activate()'>
+						<input type='text' name="pays_logement" id="country" class='adresse' required> <br><br>
+						<input type='submit' value='Valider' id='bouton'>
 				</form>
 				<p class='NB'> Tous les champs sont obligatoires.<br></p>
 			</article>
