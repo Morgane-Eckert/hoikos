aaa9cb13ed2169c55a2e86841c5742039bea5e76
PBidart <32761521+PBidart@users.noreply.github.com>
2018-01-30 13:24:11 +0100

    Update v_ajout_logement.php

--- vues/v_ajout_logement.php
+++ vues/v_ajout_logement.php
@@ -4,7 +4,6 @@
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="public/css/inscription.css">
 		<link rel="stylesheet" href="public/css/footer.css">
-		<script type="text/javascript" src="public/js/adresse.js"></script>
 	</head>
 	<body>
 		<?php include("vues/v_header_no_bouton.php");
@@ -25,6 +24,7 @@
 
 
 						<label for='telephone_fixe'> Téléphone fixe :  </label><br/>
+						<span id ="missTel"></span><br>
 						<input type='text' name='telephone_fixe' id='telephone_fixe' maxlength='10' required><br><br />
 
 						<label>Adresse</label><br>
@@ -32,16 +32,21 @@
             					onFocus="geolocate()" type="text" class='adresse' style="width:300px;"><br><br>
 
 						<label for='numero_rue_logement' id='numero_rue_logement'> Numéro de rue : </label><br/>
+						<span id ="missNumrue"></span><br>
 						<input type='text' name='numero_rue_logement' id='street_number' class='adresse' required><br><br>
 						<label for='nom_rue_logement' id='nom_rue_logement'> Nom de rue : </label><br/>
+						<span id ="missRue"></span><br>
 						<input type='text' name='nom_rue_logement' id='route' class='adresse' required><br><br>
 						<label for='code_postale_logement' id='code_postale_logement'> Code postal : </label><br/>
+						<span id ="missCp"></span><br>
 						<input type='text' name='code_postale_logement' id='postal_code' class='adresse' required><br><br>
 						<label for="ville_logement">Ville :</label><br>
+						<span id ="missVille"></span><br>
 						<input type='text' name="ville_logement" id="locality" class='adresse' required> <br><br>
 						<label for="pays_logement">Pays :</label><br>
+						<span id ="missPays"></span><br>
 						<input type='text' name="pays_logement" id="country" class='adresse' required> <br><br>
-						<input type='submit' value='Valider' id='bouton'>
+						<input type='submit' value='Valider' onclick="validation()" id='bouton'>
 				</form>
 				<p class='NB'> Tous les champs sont obligatoires.<br></p>
 			</article>
@@ -49,5 +54,7 @@
 		<?php include("vues/v_footer.php"); ?>
             <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAz2XZbaRtoXDEpEBz7QqqmMEORtzrU7Dk&libraries=places&callback=initAutocomplete"
                 async defer></script>
+								<script type='text/javascript' src='public/js/verification_adresse.js'></script>
+								<script type='text/javascript' src='public/js/adresse.js'></script>
 	</body>
 </html>
