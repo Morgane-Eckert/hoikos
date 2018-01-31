95767533eba83514d7dc9ba7ca113e3fde8d5f7e
Mike Belhomme <34034247+Azoth38@users.noreply.github.com>
2018-01-31 16:09:38 +0100

    Update v_consommation.php

--- vues/v_consommation.php
+++ vues/v_consommation.php
@@ -131,7 +131,7 @@
 							      }
                                  		         ?>
                     		 		 <div class = "BoiteVide">
-                                  		      <span class="Titre"></span><h3 class="Affichage"> <?php echo "Votre consommation d'eau s'élève à ".$donnees."" ; ?>
+                                  		      <span class="Titre"></span><h3 class="Affichage"> <?php echo "Votre consommation d'électricité s'élève à ".$donnees1."" ; ?>
                                            </h3>
 		                                    </div><br>
                                                        
