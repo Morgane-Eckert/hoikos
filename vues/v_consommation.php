e40a66db539e3f0497a566231bed157f12463014
PBidart <32761521+PBidart@users.noreply.github.com>
2018-01-31 18:22:12 +0100

    Update v_consommation.php

--- vues/v_consommation.php
+++ vues/v_consommation.php
@@ -10,11 +10,11 @@
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="/resources/demos/style.css">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script> 
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <title>Votre Consommation</title>
   </head>
 	<body>
-    <?php 
+    <?php
             include("vues/v_header_bouton.php");
             //Affichage d'un message d'erreur si un capteur ne fonctionne pas
             list($erreur_capteur,$erreur_salles,$a) = afficher_erreur_capteur();
@@ -24,7 +24,7 @@
         ?>
     <nav>
             <a href="index.php?target=compte&action=connecte&reaction=home" class="Conso">Home</a>
-             <?php //Affichage des onglets                
+             <?php //Affichage des onglets
                 $onglets = afficher_onglets();
                 if ($onglets!=NULL)
                 foreach($onglets as $element){//On parcourt le tableau
@@ -38,13 +38,18 @@
                         <?php
                     }
                 }
-             ?>
-            <a href="index.php?target=compte&action=connecte&reaction=nouvel_onglet" class="nouvel_onglet" id='nouvel_onglet'>+</a>
-            <div class="Vide"></div>
+
+							if(type_utilisateur()==1){
+								?>
+								<a href="index.php?target=compte&action=connecte&reaction=nouvel_onglet" class="nouvel_onglet" id='nouvel_onglet'>+</a>
+								<?php
+							}
+						 ?>
+					 	<div class="Vide"></div>
             <a href="index.php?target=compte&action=connecte&reaction=consommation" class="actuel">Consommations</a>
             <a href="index.php?target=compte&action=connecte&reaction=profil" class="Conso">Profil</a>
         </nav>
-				<section>	
+				<section>
 					<article>
 						<div id="titre">Consommation du foyer
             </br>
@@ -56,10 +61,10 @@
 <!-- <button onclick="javascript:randomize();">RANDOM!</button>-->
          <!-- <input type="button" name="date" id="date" value="Date" class="btn btn-info" /> -->
         </form>
-            </div> 
+            </div>
                 		    <br/><!-- Titre dans le bandeau rouge-->
                  		   <div id="corps"> <!-- Tout ce qu'il y a dans le rectangle blanc-->
-              		
+
                         		        <div class="consommation">
                        	 		             <?php //Affichage des données de consommation
 								$donnees=NULL;
@@ -70,47 +75,51 @@
                                         ?>
 
                              		       <div class = "BoiteVide">
-                               		         <span class="Titre"></span><h3 class="Affichage"><?php echo "Votre consommation d'eau s'élève à ".$donnees." "; ?>
+                               		         <span class="Titre"></span><h3 class="Affichage"><?php
+								     if($i!=0){
+																					 echo "Votre consommation d'eau s'élève à ".$donnees." ";
+								     }
+																					 ?>
                                            </h3>
                                		     </div><br>
 
                                		       <script>
-											       $(document).ready(function(){  
-											           $.datepicker.setDefaults({  
-											                dateFormat: 'dd-mm-yy'   
-											           });  
-											           $(function(){  
-											                $("#from_date").datepicker();  
-											                $("#to_date").datepicker();  
-											           });  
-											           $('#date').click(function(){  
-											                var from_date = $('#from_date').val();  
-											                var to_date = $('#to_date').val();  
-											                if(from_date != '' && to_date != '')  
-											                {  
-											                     $.ajax({  
-											                          url:"test.php",  
-											                          method:"GET",  
-											                          data:{from_date:from_date, to_date:to_date},  
-											                          success:function afficher_graph_elec(date,statut)  
-											                          {  
+											       $(document).ready(function(){
+											           $.datepicker.setDefaults({
+											                dateFormat: 'dd-mm-yy'
+											           });
+											           $(function(){
+											                $("#from_date").datepicker();
+											                $("#to_date").datepicker();
+											           });
+											           $('#date').click(function(){
+											                var from_date = $('#from_date').val();
+											                var to_date = $('#to_date').val();
+											                if(from_date != '' && to_date != '')
+											                {
+											                     $.ajax({
+											                          url:"test.php",
+											                          method:"GET",
+											                          data:{from_date:from_date, to_date:to_date},
+											                          success:function afficher_graph_elec(date,statut)
+											                          {
 											                             document.write(statut);
-											                          }  
-											                     });  
-											                }  
-											                else  
-											                {  
-											                     alert("Please Select Date");  
-											                }  
-											           });  
-											      });  
-											 
+											                          }
+											                     });
+											                }
+											                else
+											                {
+											                     alert("Please Select Date");
+											                }
+											           });
+											      });
+
 											  </script>
                                                         <div id="mydiv2"><!-- Plotly chart will be drawn inside this DIV --></div>
                                                            <script>
                                                            var trace1 = {
-                                                            x: [1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30], 
-                                                            y: [10, 15, 13, 17,14,13,12,15,18,17,5,8,7,14,12,10,11,13,16,18,9,6,7,4,12,20,17,14,15,12], 
+                                                            x: [1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
+                                                            y: [10, 15, 13, 17,14,13,12,15,18,17,5,8,7,14,12,10,11,13,16,18,9,6,7,4,12,20,17,14,15,12],
                                                             type: 'scatter',
                                                             name:'Eau(m3)',
                                                             connectgaps: true
@@ -120,9 +129,9 @@
                                                         title:"Consommation d'eau "};
                                                         Plotly.newPlot('mydiv2',data,layout);
                                                         </script>
-                                                                       		
 
-                    			<div class="consommation"> 
+
+                    			<div class="consommation">
                           		   <?php //Affichage des données de consommation
 							$donnees1=NULL;
                                 		         list($i,$donnees_recue_donnees1) = afficher_consommation_elec();
@@ -131,19 +140,23 @@
 							      }
                                  		         ?>
                     		 		 <div class = "BoiteVide">
-                                  		      <span class="Titre"></span><h3 class="Affichage"> <?php echo "Votre consommation d'électricité s'élève à ".$donnees1."" ; ?>
+                                  		      <span class="Titre"></span><h3 class="Affichage"> <?php
+																														     if($i!=0){
+																						echo "Votre consommation d'électricité s'élève à ".$donnees1."" ;
+																							}
+																						?>
                                            </h3>
 		                                    </div><br>
-                                                       
-								
-											           
+
+
+
 
                                                       </div>
 		                        		 <div id="mydiv">
                                  <script>
                                                            var trace2 = {
-                                                            x: [1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30], 
-                                                            y: [10, 15, 13, 17,14,13,12,15,18,17,13,14,18,14,12,10,11,13,16,18,15,13,10,16,12,20,17,14,15,12], 
+                                                            x: [1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
+                                                            y: [10, 15, 13, 17,14,13,12,15,18,17,13,14,18,14,12,10,11,13,16,18,15,13,10,16,12,20,17,14,15,12],
                                                             type: 'scatter',
                                                             name:'Électricité(kWh)',
                                                             connectgaps: true
@@ -153,8 +166,8 @@
                                                         title:"Consomation d'électricité "};
                                                         Plotly.newPlot('mydiv',data,layout);
                                                         </script>
-                                          
-                		
+
+
 
 
 				</article>
