0cea3dbc1cfa08d459275ad42c0124b596377022
PBidart <32761521+PBidart@users.noreply.github.com>
2018-01-31 18:23:25 +0100

    Update m_consommation.php

--- modeles/m_consommation.php
+++ modeles/m_consommation.php
@@ -7,20 +7,20 @@ function afficher_consommation_eau(){
 	$reponse->execute(array(
 		'ID_logement' => $_SESSION['ID_logement']
 		));
-	
+
 	$i=0;
 	while ($donnees = $reponse->fetch()){
-	    $consommation_eau[$i]= $donnees['donnee_recue_capteur']; 
-	    $i++; 
+	    $consommation_eau[$i]= $donnees['donnee_recue_capteur'];
+	    $i++;
 	}
-		
+
 	if($i==0){
 		return array($i,NULL);
 
 	}
 	else{
 		return array($i,$consommation_eau);
-			
+
 	}
 }
 function afficher_consommation_elec(){
@@ -31,8 +31,8 @@ function afficher_consommation_elec(){
 		));
 	$i=0;
 	while ($donnees = $reponse->fetch()){
-	    $consommation_elec[$i]= $donnees['donnee_recue_capteur']; 
-	    $i++; 
+	    $consommation_elec[$i]= $donnees['donnee_recue_capteur'];
+	    $i++;
 	}
 	if($i==0){
 		return array($i,NULL);
@@ -40,9 +40,19 @@ function afficher_consommation_elec(){
 	else{
 		return array($i,$consommation_elec);
 	}
-	
+
 }
 
+function type_utilisateur(){
+	$bdd = connexion_bdd();
+	$reponse = $bdd->prepare('SELECT type_utilisateur FROM utilisateur WHERE adresse_mail_utilisateur = :adresse');
+	$reponse->execute(array(
+		'adresse' => $_SESSION['adresse_mail_utilisateur']
+	));
+
+	$donnees = $reponse->fetch();
+	return $donnees['type_utilisateur'];
+}
 
 //$reponse=$bdd->prepare('SELECT donnee_recue_capteur from capteur where ID_logement= :ID_logement and ID_type_de_capteur =7');
 //	$reponse->execute(array(
