4486b03c159debcdf99e8f1295f72434f5b354f8
Michou <34058527+Mitisman@users.noreply.github.com>
2018-01-31 23:31:03 +0100

    Add files via upload

--- vues/v_header_no_bouton.php
+++ vues/v_header_no_bouton.php
@@ -4,12 +4,16 @@
 </head>
 
 <header>
-	<?php if((isset($_SESSION["connect"])) && ($_SESSION["connect"] == true) && ((isset($_SESSION["type_utilisateur"])) && (($_SESSION["type_utilisateur"]==1) || ($_SESSION["type_utilisateur"]==2)))) {?>
-	<a href="index.php?target=compte&action=connecte&reaction=home" class= "logoslogan">
+	<?php if ((((isset($_GET["action"])) && ($_GET['action']=='logement')) || ((isset($_GET["action"])) && ($_GET['action']=='utilisateurs_secondaires'))) && (!(isset($_GET['profil'])))){?>
+	<a href="index.php?target=inscription&action=<?php echo $_GET['action']?>" class= "logoslogan">
+	<?php }else if (isset($_GET["profil"])){?>
+	<a href="index.php?target=inscription&action=<?php echo $_GET['action']?>&profil" class= "logoslogan">
 	<?php }else if ((isset($_SESSION["connect"])) && ($_SESSION["connect"] == true) && ((isset($_SESSION["type_utilisateur"])) && (($_SESSION["type_utilisateur"])==4))) {?>
 	<a href="index.php?target=compte&action=connecte&reaction=cemac" class= "logoslogan">
 	<?php }else if ((isset($_SESSION["connect"])) && ($_SESSION["connect"] == true) && ((isset($_SESSION["type_utilisateur"])) && (($_SESSION["type_utilisateur"])==3))) {?>
 	<a href="index.php?target=compte&action=connecte&reaction=conditions_generales" class= "logoslogan">
+	<?php }else if((isset($_SESSION["connect"])) && ($_SESSION["connect"] == true) && ((isset($_SESSION["type_utilisateur"])) && (($_SESSION["type_utilisateur"]==1) || ($_SESSION["type_utilisateur"]==2)))) {?>
+	<a href="index.php?target=compte&action=connecte&reaction=home" class= "logoslogan">
 	<?php }else{ ?>
 	<a href="index.php" class= "logoslogan">
 	<?php }?>
